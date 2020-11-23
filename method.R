e38f4aab1a0cca669ec1aabc6fbd7fa5a77f445f
EmmaVJones <emma.v.jones@icloud.com>
2020-11-23 15:42:24 -0500

    bring in snapping functions

--- method.R
+++ method.R
@@ -23,10 +23,10 @@ vahu6 <- st_as_sf(pin_get("ejones/vahu6", board = "rsconnect"))
 
 # appropriate WQS layer
 if(stationType %in% c('Riverine', 'Lacustrine')){
-  WQS <- pin_get(paste0('ejones/', tolower(stationType)), board = 'rsconnect')
+  WQS <- st_as_sf(pin_get(paste0('ejones/', tolower(stationType)), board = 'rsconnect'))
 } else {
-  WQS <- pin_get('ejones/estuarinePolys', board = 'rsconnect')
-  WQSEL <- pin_get('ejones/estuarineLines', board = 'rsconnect')
+  WQS <- st_as_sf(pin_get('ejones/estuarinePolys', board = 'rsconnect'))
+  WQSEL <- st_as_sf(pin_get('ejones/estuarineLines', board = 'rsconnect'))
 }
 
 subbasinToVAHU6 <- read_csv('data/subbasinToVAHU6conversion.csv') 
@@ -34,5 +34,8 @@ subbasinToVAHU6 <- read_csv('data/subbasinToVAHU6conversion.csv')
 # Highlight HUC station falls into
 stationHUC <- st_intersection(point, vahu6) %>%
   left_join(dplyr::select(subbasinToVAHU6, VAHU6, Basin, BASIN_CODE, Basin_Code))
-wqs_HUC <- WQS[stationHUC,]
+
+# use snapping tools to get closest WQS for user
+wqs_HUC <- st_intersection(point, WQS)
+
 vafrm_HUC <- vafrm99_05[stationHUC,]
